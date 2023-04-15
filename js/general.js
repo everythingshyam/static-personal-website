2637fba838da58c8e02f7937def4ed138921abd8
Shyam Sundar Tiwari <shyampersonal2000@gmail.com>
2023-04-16 03:57:43 +0530

    added message form

--- js/general.js
+++ js/general.js
@@ -12,4 +12,16 @@ function menu_click() {
     menu_click_count = 0;
   }
   //   menu_click_count += 1;
+
+}
+function sendMessage() {
+  Email.send({
+    Host: "smtp.elasticmail.com",
+    Username: "shyamsundar.tiwari20@pccoepune.org",
+    Password: "AD5E6806E3D2D6512667C8FFB331E6E6383F",
+    To: "s1024creative@gmail.com",
+    From: document.getElementById("email").value,
+    Subject: "New message from Portfolio Website",
+    Body: document.getElementById('message').value,
+  }).then((message) => alert(message));
 }
